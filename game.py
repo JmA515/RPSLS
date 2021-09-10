136d3f4882e00ded707ad2609875c1ff990a29a3
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-09-10 15:49:56 -0500

    Pair programmed with JP: Added the welcome message along with game rules to Game class.

---
+++ game.py
@@ -0,0 +1,25 @@
+from human import Human
+from ai import Artificial
+
+class Game:
+    def __init__(self):
+        pass
+
+    def welcome_rules(self):
+        print("Welcome to Rock, Paper, Scissors, Lizard, Spock (RPSLS)!\n"
+        "\n"
+        "The rules of RPSLS is:\n"
+        "\n"
+        "Rock crushes Scissors\n"
+        "Scissors cuts Paper\n"
+        "Paper covers Rock\n"
+        "Rock crushes Lizard\n"
+        "Lizard poisons Spock\n"
+        "Spock smashes Scissors\n"
+        "Scissors decapitates Lizard\n"
+        "Lizard eats Paper\n"
+        "Paper disproves Spock\n"
+        "Spock vaporizes Rock")
+    
+    def run_game(self):
+        pass
