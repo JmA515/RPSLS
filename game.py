ea71282ac6dca7cfc05f936800b1197dd42f63ed
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-09-13 16:46:41 -0500

    Pair programmed with JP. Added the human game mode method (aka. multiplayer).

--- game.py
+++ game.py
@@ -48,7 +48,7 @@ class Game:
         human_winner_counter = 0
         ai_winner_counter = 0
 
-        human_round1 = self.human1_turn()
+        human_round1 = self.human1_turn(1)
         ai_round1 = self.ai_turn()
         round_one_winner = self.find_round_winner(human_round1, ai_round1)
         print(round_one_winner)
@@ -57,7 +57,7 @@ class Game:
         elif round_one_winner == f'{self.contestants[0].name} wins!':
             ai_winner_counter += 1
         
-        human_round2 = self.human1_turn()
+        human_round2 = self.human1_turn(1)
         ai_round2 = self.ai_turn()
         round_two_winner = self.find_round_winner(human_round2, ai_round2)
         print(round_two_winner)
@@ -70,7 +70,7 @@ class Game:
         elif ai_winner_counter == 2:
             print(f'{self.contestants[0].name} wins!!!\n''Better luck next time! ¯\_(ツ)_/¯')
         elif human_winner_counter < 2 and ai_winner_counter < 2:
-            human_round3 = self.human1_turn()
+            human_round3 = self.human1_turn(1)
             ai_round3 = self.ai_turn()
             round_three_winner = self.find_round_winner(human_round3, ai_round3)
             print(round_three_winner)
@@ -95,13 +95,57 @@ class Game:
         print(ai_player_choice)
         return ai_player_choice
 
-    def human1_turn(self):
-        self.contestants[1].choose_gesture()
+    def human1_turn(self, index_number):
+        self.contestants[index_number].choose_gesture()
         human1_choice = self.valid_answer()
         print(human1_choice)
         return human1_choice
 
     def human_game(self):
+        human1_winner_counter = 0
+        human2_winner_counter = 0
+
+        human1_round1 = self.human1_turn(1)
+        human2_round1 = self.human1_turn(0)
+        round_one_winner = self.find_round_winner(human1_round1, human2_round1)
+        print(round_one_winner)
+        if round_one_winner == f'{self.contestants[1].name} wins!':
+            human1_winner_counter += 1
+        elif round_one_winner == f'{self.contestants[0].name} wins!':
+            human2_winner_counter += 1
+        
+        human1_round2 = self.human1_turn(1)
+        human2_round2 = self.human1_turn(0)
+        round_two_winner = self.find_round_winner(human1_round2, human2_round2)
+        print(round_two_winner)
+        if round_two_winner == f'{self.contestants[1].name} wins!':
+            human1_winner_counter += 1
+        elif round_two_winner == f'{self.contestants[0].name} wins!':
+            human2_winner_counter += 1
+        if human1_winner_counter == 2:
+            print(f'{self.contestants[1].name} wins!!!')
+        elif human2_winner_counter == 2:
+            print(f'{self.contestants[0].name} wins!!!')
+        elif human1_winner_counter < 2 and human2_winner_counter < 2:
+            human1_round3 = self.human1_turn(1)
+            human2_round3 = self.human1_turn(0)
+            round_three_winner = self.find_round_winner(human1_round3, human2_round3)
+            print(round_three_winner)
+            if round_three_winner == f'{self.contestants[1].name} wins!':
+                human1_winner_counter += 1
+            elif round_three_winner == f'{self.contestants[0].name} wins!':
+                human2_winner_counter += 1
+            
+            if human1_winner_counter >= 2:
+                print(f'{self.contestants[1].name} wins!!!')
+            elif human2_winner_counter >= 2:
+                print(f'{self.contestants[0].name} wins!!!')
+            elif human1_winner_counter == 1 and human2_winner_counter == 0:
+                print(f'{self.contestants[1].name} wins!!!') 
+            elif human2_winner_counter == 1 and human1_winner_counter == 0:
+                print(f'{self.contestants[0].name} wins!!!')
+            elif human1_winner_counter == human2_winner_counter:
+                print("How does it feel to tie and win nothing?! Sucks right?\n""Play again because if you ain't first, you're last!")
         pass        
 
     def run_game(self):
